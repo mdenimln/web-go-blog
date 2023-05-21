6fa84a933399e6ee76a643afec3bd1841999197e
Deni Maulana <denimlnm@gmail.com>
2023-05-21 22:27:08 +0700

    up contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import './Contact.css';
 export default function Contact() {
     
     return(
-        <>
-
-        <h1>Halaman Contact</h1>
-        <ul>
-            <li><a href='facebook.com'>facebook</a></li>
-            <li><a href='facebook.com'>instagram</a></li>
-            <li><a href='facebook.com'>watsaap</a></li>
-        </ul>
-        </>
+        <section style={{display: 'flex'}}>
+            <div style={{position: 'relative', width: '30%', border: '1px solid'}}>
+            <div style={{width: '27%', border: '1px solid',position: 'fixed'}}>
+                <h1>judul</h1>
+            </div>
+            </div>
+            
+            <div className="parent">
+                <div className="div1 div"> </div>
+                <div className="div2 div"> </div>
+                <div className="div3 div"> </div>
+                <div className="div4 div"> </div>
+                <div className="div5 div"> </div>
+            </div>
+        </section>
     )
 }
